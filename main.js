d82b5a9023e1b140fc1894c958edfd558a09ecd5
Nightburgerus <Nightburgerus>
2022-06-05 17:37:24 +0300

    Add row's generation

--- main.js
+++ main.js
@@ -11,3 +11,22 @@ let employees = [
     new Employee("Сергеев Алекстандр Степанович", "13.04.1967", "Заведующий лабораторией"),
     new Employee("Авдеев Леонид Андреевич", "07.11.1982", "Технолог"),
 ];
+
+let table = document.getElementsByClassName("employee-list")[0]
+
+for (let i = 0; i < employees.length; i++ ){
+    let row = document.createElement("tr")
+    let name = document.createElement("td")
+    let birth = document.createElement("td")
+    let position = document.createElement("td")
+
+    name.textContent = employees[i].name
+    birth.textContent = employees[i].birthday
+    position.textContent = employees[i].position
+
+    row.appendChild(name)
+    row.appendChild(birth)
+    row.appendChild(position)
+
+    table.appendChild(row)
+}
