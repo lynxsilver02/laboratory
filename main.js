06220daa224e58321307877f4c748df5d207c14a
Anastasia <lynxsilver02@gmail.com>
2022-06-05 17:19:44 +0300

    + main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+class Employee {
+    constructor(name, birthday, position) {
+        this.name = name;
+        this.birthday = birthday;
+        this.position = position;
+    }
+}
+
+let employees = [
+    new Employee("Иванов Иван Иванович", "24.06.1987", "Технолог"),
+    new Employee("Сергеев Алекстандр Степанович", "13.04.1967", "Заведующий лабораторией"),
+    new Employee("Авдеев Леонид Андреевич", "07.11.1982", "Технолог"),
+];
